cd64df3d280ca0c5bee39a4fc0c4020a51cf0256
d1mon_gk <ddedshout@gmail.com>
2026-06-17 18:08:55 +0300

    Add: majority-element - submission-2

---
+++ Data Structures & Algorithms/majority-element/submission-2.py
@@ -0,0 +1,10 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        ans = count = 0
+
+        for num in nums:
+            if count == 0:
+                ans = num
+            count += (1 if ans == num else -1)
+        return ans
+        
